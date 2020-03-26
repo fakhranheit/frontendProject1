6316b64c3bad5e70425ff8ef6abe154baf385eef
fakhranheit <fakhranwahyu@gmail.com>
2020-03-26 21:54:40 +0700

    search fixed

--- project/projectsaya/src/components/cardStore.jsx
+++ project/projectsaya/src/components/cardStore.jsx
@@ -4,6 +4,8 @@ import { APIURL, APIURLImg } from '../helper/apiurl'
 import { Link } from 'react-router-dom'
 import { CustomInput } from 'reactstrap'
 import AOS from "aos";
+import SearchIcon from '@material-ui/icons/Search';
+import { Button } from 'react-bootstrap'
 
 class CardStore extends Component {
     state = {
@@ -11,6 +13,33 @@ class CardStore extends Component {
         page: 1,
         pager: {},
         search: ''
+    }
+
+    searchGame = () => {
+        var search = this.state.search
+        if (search !== '') {
+            this.setState({ page: 1 })
+            console.log('searched game', search);
+            Axios.post(`${APIURL}game/searchgame/${this.state.page}`, { search })
+                .then(res => {
+                    console.log(res.data);
+                    this.setState({ dataGame: res.data.pageOfData, pager: res.data.pager })
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        } else {
+            console.log('masuk all game');
+            Axios.get(`${APIURL}game/getstore/${this.state.page}`)
+                .then(res1 => {
+                    console.log('get game', res1.data)
+                    this.setState({ dataGame: res1.data.pageOfData, pager: res1.data.pager })
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+
     }
 
     renderProduk = () => {
@@ -47,9 +76,18 @@ class CardStore extends Component {
 
 
     componentDidUpdate(_, prevState) {
-        var search = this.state.search
-        if (search !== '') {
-            console.log('searched game', search);
+        if (prevState.page !== this.state.page && this.state.search == '') {
+            Axios.get(`${APIURL}game/getstore/${this.state.page}`)
+                .then(res1 => {
+                    console.log('get game', res1.data)
+                    this.setState({ dataGame: res1.data.pageOfData, pager: res1.data.pager })
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+        if (prevState.page !== this.state.page && this.state.search !== '') {
+            var search = this.state.search
             Axios.post(`${APIURL}game/searchgame/${this.state.page}`, { search })
                 .then(res => {
                     console.log(res.data);
@@ -58,25 +96,10 @@ class CardStore extends Component {
                 .catch(err => {
                     console.log(err);
                 })
-        } else {
-            console.log('all game', search);
-            if (prevState.page !== this.state.page) {
-                Axios.get(`${APIURL}game/getstore/${this.state.page}`)
-                    .then(res1 => {
-                        console.log('get game', res1.data)
-                        this.setState({ dataGame: res1.data.pageOfData, pager: res1.data.pager })
-                    })
-                    .catch(err => {
-                        console.log(err)
-                    })
-            }
         }
-
-
     }
 
     render() {
-        console.log(this.state.search);
         AOS.init({
             delay: 5000
         });
@@ -86,6 +109,9 @@ class CardStore extends Component {
             <div data-aos="fade-up" style={{ fontFamily: 'Oxanium', color: 'white' }}>
                 <h1 style={{ display: 'flex', justifyContent: 'center', paddingTop: '100px' }}>
                     <CustomInput placeholder="search the game here" value={this.state.search} onChange={(e) => this.setState({ search: e.target.value })} />
+                    <Button variant='dark' onClick={this.searchGame} >
+                        <SearchIcon fontSize='large' />
+                    </Button>
                 </h1>
                 <div className="box-card-store row ">
                     {this.renderProduk()}
